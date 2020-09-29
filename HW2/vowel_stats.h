ee4b1796e738a869cc8119bbd3574557c70584e4
ryan1tran <mastersword3459@gmail.com>
2020-09-29 14:39:38 -0700

    Assignment 2 Completed

---
+++ HW2/vowel_stats.h
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+#ifndef VS_H
+#define VS_H
+
+#include <vector>
+#include <string>
+
+void get_vector_stats(std::vector<std::string> vect);
+void get_array_stats(std::string arr[], int arr_length);
+void display_stats();
+
+#endif
+#endif
